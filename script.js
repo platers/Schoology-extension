c1a7c3476037c4a3a3c494f0f4d478ad9bc8d75a
jordanlee008 <jordanlee008@gmail.com>
2017-02-06 10:26:36 -0500

    Blue background and button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.body.style.backgroundColor = "black";
+document.body.style.backgroundColor = "#6699ff";
 var button = document.createElement("BUTTON");
 var t = document.createTextNode("CLICKME");
 button.appendChild(t);
