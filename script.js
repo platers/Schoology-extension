e04b87db884a9b0eeed0c2bd1049f176d0e6f83e
jordanlee008 <jordanlee008@gmail.com>
2017-03-09 10:07:33 -0500

    Added border and debugging

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 document.body.style.backgroundColor = "#6699ff";
-var assignment = document.getElementsByClassName("course-event");
-console.log(assignment.length);
-//sleep(7000); 
+
+var sidebar = document.querySelectorAll(".upcoming-events");
+var list = document.querySelector(".upcoming-list");
+var children = list.childNodes;
+var assignment = document.querySelectorAll(".course-event");
+console.log("sidebar: " + sidebar.length);
+console.log("list: " + list.classList)
+console.log("children: " + list.childElementCount);
+console.log("assignment: " + assignment.length);
+
+sidebar[0].style.border = "10px solid #ffaa66";
+
 for (var i = 0; i < assignment.length; i++) {
     var button = document.createElement("INPUT");
-    //button.setAttribute("type", "button");
-    //button.setAttribute("value", "Color");
+    button.setAttribute("type", "button");
+    button.setAttribute("value", "Button");
     
     assignment[i].style.backgroundColor = "#00FA9A";
     assignment[i].append(button);
 }
-
-var button = document.createElement("BUTTON");
-var t = document.createTextNode("CLICKME");
-button.appendChild(t);
-document.body.appendChild(button);
 
